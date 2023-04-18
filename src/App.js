2f07a36ab12e145e93a9cade0e23d6352cb8811d
MichelRLima <michelrocha502@gmail.com>
2023-04-17 22:43:05 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,74 @@
-
+import React, { useState } from "react";
 import './App.css';
 
+
+
+
+
 function App() {
+const [todos, setTodos] = useState([]);
+const [inputValue, setInputValue] = useState ("")
+
+function addTodo (){ //funcao para adicionar tarefa no array
+    if (inputValue !== ""){
+      setTodos([...todos, {task:inputValue, completed: false}]) //adiciona o objeto da tarfa no array
+      setInputValue(""); //apagar o que foi escrito no input
+    }
+}
+
+const completeTodo = (index) =>{ //funcao para marcar como completa a tarefa
+  const finishTodos = [...todos] //pegando todas as tarefas de "todos"
+  if(finishTodos[index].completed == false){
+    
+    finishTodos[index].completed = true; //adicionado como True no index da tarefa marcada como completada
+  }else{
+
+    finishTodos[index].completed = false; //adicionado como True no index da tarefa marcada como completada
+  }
+    setTodos(finishTodos); //retornar para o array "todos" as tarefas com o atributo "completd" atualizado
+}
+
+const removerTodo = (index) =>{
+    const deleteTodo = [...todos] //pegando todas as tarefas de "todos"
+    deleteTodo.splice(index,1) //deletenando apenas a tarefa que foi acionada
+    setTodos(deleteTodo) //retornar para o array "todos" as tarefas atualizadas
+}
+
   return (
     <div className="App">
-      <h1>teste</h1>
+
+      <div className="titulo">
+        <h1>Todo List</h1>
+        <p>Criado por <a target="_blanck" href="https://www.linkedin.com/in/michel-rocha-01b550210/">Michel Rocha</a> em React</p>
+      </div>
+
+      <div className="ContainerTodoList">
+        <h2>Qual será sua tarefa?</h2>
+        <div className="ContainerTarefa">
+        <input type="text" 
+        placeholder="Digite sua tarefa" 
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}></input>
+
+        <input type="submit" value="Criar tarefa" onClick={addTodo}></input>
+        </div>
+        <div>
+          {todos.map((todo, index) =>(
+            <div key={index} className="Todo">
+          
+
+             <h3 style={{textDecoration: todo.completed ? "line-through" : "" }}>{todo.task}</h3> {/* foi adicionado um estilo para quando for completado a tarefa */}
+              
+              <div className="acoes">
+                <button onClick={() => removerTodo(index)}>Remover</button>{/* botao para acionar a funcao removerTodo */}
+
+                <button onClick={() => completeTodo(index)}>Concluir</button>{/* botao para acionar a funcao CompleteTodo*/}
+              </div>
+              
+              </div> 
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
